5862b64f310383131e76bc8078f8e79c3541e641
Ajay-1502 <ahdesai40@gmail.com>
2025-07-12 15:37:13 +0530

    Loading the application

--- src/App.js
+++ src/App.js
@@ -4,24 +4,10 @@ import './App.css';
 
 function App() {
   const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   async function fetchApiHandler() {
-    /*fetch('https://swapi.py4e.com/api/films')
-      .then((response) => {
-        return response.json();
-      })
-      .then((data) => {
-        const transformedMovies = data.results.map((movie) => {
-          return {
-            id: movie.id,
-            title: movie.title,
-            releaseDate: movie.release_date,
-            openingText: movie.opening_crawl,
-          };
-        });
-        setMovies(transformedMovies);
-      });*/
-
+    setIsLoading(true);
     try {
       const response = await fetch('https://swapi.py4e.com/api/films');
       const data = await response.json();
@@ -34,6 +20,7 @@ function App() {
         };
       });
       setMovies(transformedMovies);
+      setIsLoading(false);
     } catch {
       alert('Error in fetching the details');
     }
@@ -45,7 +32,11 @@ function App() {
         <button onClick={fetchApiHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={movies} />
+        {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
+        {!isLoading && movies.length === 0 && (
+          <p>There are no movies to display</p>
+        )}
+        {isLoading && <p>Loading...</p>}
       </section>
     </React.Fragment>
   );
