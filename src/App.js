849c83b97a7c00b555f561c8c9e0ef04973004d5
Ajay-1502 <ahdesai40@gmail.com>
2025-07-13 17:57:08 +0530

    Optimised code using useCallback hook

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import MoviesList from './components/MoviesList';
 import './App.css';
 
@@ -8,11 +8,7 @@ function App() {
   const [error, setError] = useState(null);
   const [shouldRetry, setShouldRetry] = useState(true);
 
-  useEffect(() => {
-    fetchApiHandler();
-  }, []);
-
-  async function fetchApiHandler() {
+  const fetchApiHandler = useCallback(async () => {
     setIsLoading(true);
     setError(null);
 
@@ -38,7 +34,11 @@ function App() {
       setError(error.message);
     }
     setIsLoading(false);
-  }
+  }, []);
+
+  useEffect(() => {
+    fetchApiHandler();
+  }, [fetchApiHandler]);
 
   useEffect(() => {
     let retryTimer;
